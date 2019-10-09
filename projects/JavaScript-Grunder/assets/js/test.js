83c8e89f9ded5e988073bdacf7b2573ceae84b68
Jonas Backlund <jonaback1@lycksele.se>
2019-10-09 12:39:45 +0200

    Added: for loops

--- projects/JavaScript-Grunder/assets/js/test.js
+++ projects/JavaScript-Grunder/assets/js/test.js
@@ -1,21 +1,20 @@
 // alert("Pop-up-ruta")
 
+/* 
 var myName = "Jonas"; // Datatyp: String
 var myAge = 18; // Datatyp: Number
 var isEvil = false; // Datatyp: Boolean
 
 //  Single line comment
-/*
+
     *************
     Block Comment
     *************
-*/
 
-/*
     ---------------
     |   if/else   |
     ---------------
-*/
+
 
 if (myAge < 18) {
     // alert("Sad")
@@ -43,11 +42,11 @@ if (isEvil == true) {
     //alert("Du är god.");
 }
 
-/* 
+
     ******************
     |  Guessing app  |
     ******************
-*/
+
 
 var secretNumber = 7;
 var userInput = Number(prompt("Guess a number between 0 - 10"));
@@ -62,4 +61,35 @@ else if (userInput < secretNumber) {
 }
 else if (userInput > secretNumber) {
     alert("Du gissar för högt");
+}
+*/
+
+
+var choices = ["Nudlar", "Pankaka", "Palt","Tacos", "Köttbullar", "Pizza"];
+
+/*  for (var i = 0; i < choices.length; i++) {
+    document.writeln('My nr ' + (i + 1) + '     favorite food is ' + choices[i]);
+    document.writeln('<br/>');
+}
+
+
+for (var i = 0; i < choices.length; i++) {
+    if ((i+1) == 1) {
+        var ordinal = "st ";
+    } else if ((i+1) == 2) {
+        var ordinal = "nd ";
+    } else if ((i+1)== 3) {
+        var ordinal = "rd ";
+    } else {
+        var ordinal = "th ";
+    }
+    document.writeln('My '+ (i + 1) + ordinal + ' favorite food is ' + choices[i]);
+    document.writeln('<br/>');
+
+*/
+for (var i = 10; i > 1 ; i--) {
+    document.writeln('hej');
+    for(var = b; 10 < b; b--) {
+        document.writeln('* ');
+    document.writeln('<br/>');
 }
