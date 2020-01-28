95773b129b4853e3bf3b0870c83bedfaeb04bc6b
jonas backlund <zohiigh@hotmail.com>
2020-01-28 14:00:18 +0100

    Added: 6_objects.js

---
+++ projects/JavaScript-Grunder/assets/js/6_objects.js
@@ -0,0 +1,12 @@
+var output = document.querySelector('body > p');
+var person = ['Sid', 31, 'EagleShieldBay'];
+
+output.innerHTML = person[2];
+
+var persona = {
+    name: "Sid", // key/value pair
+    age: 41,
+    hometown: "Heresneezedduck"
+};
+
+output.innerHTML = persona.hometown;
